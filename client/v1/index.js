fc2ce44c4834b3c5b5352669062a096ff4bac191
slender1128 <jb.rousselle@gmail.com>
2022-01-17 14:17:50 +0100

    Sort by date
    TODO : Sort by date done.

--- client/v1/index.js
+++ client/v1/index.js
@@ -80,7 +80,16 @@ console.table(marketplace_by_price);
 // 1. Create a function to sort the marketplace objects by products date
 // 2. Create a variable and assign it the list of products by date from recent to old
 // 3. Log the variable
+function sort_by_date(products) {
+	return products.sort((product1, product2) => {
+		var date1 = new Date(product1.date);
+		var date2 = new Date(product2.date);
+		return date2.getTime() - date1.getTime();
+	});
+}
 
+var marketplace_by_date = sort_by_date(marketplace);
+console.table(marketplace_by_date);
 
 // 🎯 TODO: Filter a specific price range
 // 1. Filter the list of products between 50€ and 100€
