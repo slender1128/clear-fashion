19cef757d8318f94016cd04591b2705368653da9
slender1128 <jb.rousselle@gmail.com>
2022-03-12 14:30:35 +0100

    Get product by id

--- server/api.js
+++ server/api.js
@@ -1,6 +1,9 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
+require('dotenv').config();
+const {MongoClient} = require('mongodb');
+const db = require('./db');
 
 const PORT = 8092;
 
@@ -16,6 +19,12 @@ app.options('*', cors());
 
 app.get('/', (request, response) => {
   response.send({'ack': true});
+});
+
+app.get('/products/:id', async (request, response) => {
+  const id = parseInt(request.params.id);
+  const product = await db.find({'_id' : id});
+  response.status(200).json({'result' : product[0]});
 });
 
 app.listen(PORT);
