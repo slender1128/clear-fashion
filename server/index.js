f352c941590dd2eddc4e03f56d6c488310be0446
slender1128 <jb.rousselle@gmail.com>
2022-03-22 14:51:50 +0100

    Vercel

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const root = require("./api/root");
 const search = require("./api/search");
-const products_id = require("./api/products_id");
 
 const app = express();
 
-const PORT = process.env.PORT || 8092;
+const PORT = process.env.PORT || 27017;
 
 app.use('/', root);
 app.use('/products/search', search);
-app.use('/products/:id', products_id);
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
